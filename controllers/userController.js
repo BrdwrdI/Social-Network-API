c91b1f0c2124ac4fcb847aea032007da94f31eea
WRD <dandobilly@gmail.com>
2024-03-23 19:12:14 -0400

    added update route to userController

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require('../models/User')
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUserId(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId }).select('-__v');
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!'});
+            }
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    
+}
