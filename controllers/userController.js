a793f75edcd8ac7f5e745b396588bf94da199444
WRD <dandobilly@gmail.com>
2024-03-23 20:03:16 -0400

    added deleteUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,14 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    async createUser(req,res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
     async updateUser(req, res) {
         try {
             const user = await User.findOneAndUpdate(
@@ -36,5 +44,16 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+            if(!user) {
+                return res.status(404).json({ message: 'No user with this id!'});
+            }
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err)
+        }
+    }
 }
